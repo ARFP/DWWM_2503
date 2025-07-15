d9f6728ad4f524cc773d59a76bcf412bb6012ecd
MDevoldere206 <10086604+mdevoldere@users.noreply.github.com>
2025-07-15 11:37:55 +0200

    taille texte version algo

--- web-frontend-js/02-taille-texte/exo2.js
+++ web-frontend-js/02-taille-texte/exo2.js
@@ -1,32 +1,61 @@
 
 const btnIncrease = document.getElementById('btnIncrease');
+const btnDecrease = document.getElementById('btnDecrease');
 const txtSize = document.querySelector('#txtSize'); 
 const txt = document.getElementById('txt');
 
 
-function increaseTextSize(event) {
-    console.log(this);
-    console.log(event);
-
-    let fontSize = parseInt(txtSize.textContent);
-
-    /*if(isNaN(fontSize)) {
-        console.error('nombre invalide');
-        return;
-    }*/
-
-    /*console.log(typeof(fontSize));
-
-    if(!(fontSize instanceof Number)) {
-        console.error('nombre invalide');
-        return;
-    }
-
-    console.log(fontSize);*/
-
+function applyTextStyle(myFontSize) {
+    txtSize.textContent = myFontSize; // on réinjecte la valeur dans le DOM
+    txt.style.fontSize = myFontSize + 'px'; // On appplique la taille au texte en modifiant son style 
 }
 
+function checkFontSize() {
+    let myFontSize = txtSize.textContent; // récupère la valeur dans le <span id="txtSize">
+    myFontSize = parseInt(myFontSize); // conversion en nombre
+
+    if(isNaN(myFontSize)) { // si myFontSize n'est pas un nombre valide
+        console.error('nombre invalide');
+        alert('Nombre invalide ! ');
+        return 16;
+    }
+
+    return myFontSize;
+}
+
+function increaseTextSize() {
+
+    // appel de la fonction checkFontSize
+    let myFontSize = checkFontSize();
+  
+    if(myFontSize < 48) { 
+        myFontSize++; // incrémente la valeur
+    } else {
+        myFontSize = 16;
+    }
+
+    applyTextStyle(myFontSize);
+}
+
+function decreaseTextSize() {
+    // appel de la fonction checkFontSize
+    let myFontSize = checkFontSize();
+
+  
+    if(myFontSize > 8) { 
+        myFontSize--; // incrémente la valeur
+    } else {
+        myFontSize = 16;
+    }
+
+    applyTextStyle(myFontSize);
+}
+
+
+
+
 btnIncrease.addEventListener('click', increaseTextSize);
+btnDecrease.addEventListener('click', decreaseTextSize);
 
 
 
