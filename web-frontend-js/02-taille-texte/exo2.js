671eb396073cc665ccb807767e65f0ff26fb09e8
MDevoldere206 <10086604+mdevoldere@users.noreply.github.com>
2025-07-15 12:34:36 +0200

    exo taille texte fini

--- web-frontend-js/02-taille-texte/exo2.js
+++ web-frontend-js/02-taille-texte/exo2.js
@@ -5,57 +5,43 @@ const txtSize = document.querySelector('#txtSize');
 const txt = document.getElementById('txt');
 
 
-function applyTextStyle(myFontSize) {
-    txtSize.textContent = myFontSize; // on réinjecte la valeur dans le DOM
-    txt.style.fontSize = myFontSize + 'px'; // On appplique la taille au texte en modifiant son style 
-}
+function changeTextSize(event) {
 
-function checkFontSize() {
-    let myFontSize = txtSize.textContent; // récupère la valeur dans le <span id="txtSize">
+    console.log(event.target.id); // quel élément a déclenché l'évènement 
+
+    let btnId = event.target.id;
+
+    let myFontSize = txtSize.value; // récupère la valeur dans le <input id="txtSize">
     myFontSize = parseInt(myFontSize); // conversion en nombre
 
     if(isNaN(myFontSize)) { // si myFontSize n'est pas un nombre valide
         console.error('nombre invalide');
         alert('Nombre invalide ! ');
-        return 16;
+        return;
     }
-
-    return myFontSize;
-}
-
-function increaseTextSize() {
-
-    // appel de la fonction checkFontSize
-    let myFontSize = checkFontSize();
   
-    if(myFontSize < 48) { 
+    if(myFontSize >= 8 && myFontSize <= 48 && btnId === "txtSize") {
+        // Do Nothing !
+    }
+    else if(myFontSize > 8 && myFontSize < 48 && btnId === "btnIncrease") { 
         myFontSize++; // incrémente la valeur
+    } 
+    else if (myFontSize > 8 && myFontSize < 48 && btnId === "btnDecrease") {
+        myFontSize--;
     } else {
         myFontSize = 16;
     }
 
-    applyTextStyle(myFontSize);
-}
-
-function decreaseTextSize() {
-    // appel de la fonction checkFontSize
-    let myFontSize = checkFontSize();
-
-  
-    if(myFontSize > 8) { 
-        myFontSize--; // incrémente la valeur
-    } else {
-        myFontSize = 16;
-    }
-
-    applyTextStyle(myFontSize);
+    txtSize.value = myFontSize; // on réinjecte la valeur dans le DOM
+    txt.style.fontSize = myFontSize + 'px'; // On appplique la taille au texte en modifiant son style 
 }
 
 
 
 
-btnIncrease.addEventListener('click', increaseTextSize);
-btnDecrease.addEventListener('click', decreaseTextSize);
+btnIncrease.addEventListener('click', changeTextSize);
+btnDecrease.addEventListener('click', changeTextSize);
+txtSize.addEventListener('change', changeTextSize);
 
 
 
