7b9aab9b5327ca700e635a2f1fc868cdbda35ba1
MDevoldere206 <10086604+mdevoldere@users.noreply.github.com>
2025-07-23 10:30:49 +0200

    ideas uidata

--- web-frontend-js/00-exemples/03-createElement.js
+++ web-frontend-js/00-exemples/03-createElement.js
@@ -3,6 +3,18 @@ const addItem = document.getElementById('addItem');
 const clearItems = document.getElementById('clearItems');
 const myList = document.querySelector('#myList');
 
+let toto = document.getElementById('toto');
+console.log(toto.dataset);
+
+const myIdeas = ['Element REF-1', 'Element REF', 'Element REF+1'];
+/*
+myIdeas.push('Element Push'); // push = ajoute un élément à la fin du tableau
+myIdeas.unshift('Element Unshift'); // unshift = ajoute un élément au début du tableau
+myIdeas.shift();// supprimer le premier du tableau
+myIdeas.pop(); // Supprimer le dernier élément du tableau
+console.log(myIdeas);
+*/
+
 /**
 * Ajoute la valeur saisie dans le champ texte à la section myList
 * */
@@ -12,15 +24,49 @@ function addIdea() {
    let newItemValue = newItem.value;
    console.log(newItemValue);
 
-   let p = document.createElement('p');
-   p.classList.add('toto');
-   p.textContent = newItemValue;
-   myList.append(p);
+   // ajoute la valeur au tableau MyIdeas
+   myIdeas.push(newItemValue);
+   console.log(myIdeas);
+   refreshUi();
+}
 
-   let btn = document.createElement('button');
-   btn.textContent = "Supprimer";
-   btn.type = "button";
-   p.append(btn);
+function deleteIdea(event) {
+
+   
+   let btn = event.target; // récupérer le bouton qui a déclenché l'évènement
+   let position = btn.dataset.position;
+   console.log(btn, position);
+
+   myIdeas.splice(position, 1);
+
+   // supprime un élément spécifique d'un tableau !
+   //myIdeas.filter(); // filtrer le tableau en supprimant l'élément à supprimer
+   //myIdeas.splice(); // supprime un élément du tableau à un index spécifique
+
+   console.log(myIdeas);
+
+   refreshUi();
+
+}
+
+function refreshUi() {
+
+   myList.innerHTML = '';
+
+   for(let i = 0; i < myIdeas.length; i++) {
+
+      let p = document.createElement('p'); // créer une balise <p>
+      p.textContent = myIdeas[i]; // ajoute la valeur courante du tableau dans la balise <p>
+      myList.append(p); // insère la balise <p> dans la <section id="myList">
+
+      let btn = document.createElement('button'); // <button></button>
+      btn.textContent = "Supprimer";  // <button>Supprimer</button>
+      btn.type = "button"; //  // <button type="button">Supprimer</button>
+      btn.dataset.position = i;
+      btn.addEventListener('click', deleteIdea);
+      p.append(btn);
+
+   }
 
 }
 
