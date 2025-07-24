5214dac8279017e6541c5d2b80c03f1eb1932848
MDevoldere206 <10086604+mdevoldere@users.noreply.github.com>
2025-07-24 11:58:29 +0200

    exemple separation CDA

--- web-frontend-js/00-exemples/04-functions.js
+++ web-frontend-js/00-exemples/04-functions.js
@@ -1,37 +1,15 @@
+import { calculateSum } from './04-functions-lib.js';
+
 const userInput = document.getElementById('userInput');
 const validate = document.getElementById('validate');
 const result = document.getElementById('result');
 
-validate.addEventListener('click', addValue);
-
-
+validate.addEventListener('click', eventBtnValidate);
 
 /**
  * Affiche le résultat du calcul dans la page web
- * Invoque la fonction calculate()
- * Invoque la fonction saveToBrowser
  */
-function addValue() {
-    let n = calculate(); 
-    saveToBrowser(n);
-    result.textContent = n;
-}
-
-/**
- * Additionne les valeurs du champs userInput et du paragraphe result
- * @returns {Number} Le résultat du calcul
- */
-function calculate() {
-    let currentValue = parseInt(result.textContent);
-    let valueToAdd = parseInt(userInput.value);
-    let finalValue = currentValue + valueToAdd;
-    return finalValue;
-}
-
-/**
- * Sauvegarde une valeur dans le stockage local du navigateur
- * @param {Number} valueToSave La valeur à sauvegarder
- */
-function saveToBrowser(valueToSave) {
-    localStorage.setItem('monNombre', valueToSave);
+function eventBtnValidate() {
+    let finalValue =  calculateSum(userInput.value, result.textContent);
+    result.textContent = finalValue;
 }
