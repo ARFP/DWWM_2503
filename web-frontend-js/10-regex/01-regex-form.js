9bb078611c3ff303f87378a30d3917ec683ef501
MDevoldere205 <10086604+mdevoldere@users.noreply.github.com>
2025-08-26 10:59:32 +0200

    regex ex

--- web-frontend-js/10-regex/01-regex-form.js
+++ web-frontend-js/10-regex/01-regex-form.js
@@ -1,7 +1,47 @@
 const myForm = document.getElementById('myForm');
+const myName = document.getElementById('myName');
+const myBirthDate = document.getElementById('myBirthDate');
+const myPhone = document.getElementById('myPhone');
+const myPassword1 = document.getElementById('myPassword1');
+const myPassword2 = document.getElementById('myPassword2');
 
 myForm.addEventListener('submit', function (event) {
     event.preventDefault();
-    alert('Formulaire soumis !');
+
+    // controles de saisies
+
+    const isNameValid = validateName(myName.value);
+
+    if(true) {
+        alert('Formulaire valide !');
+    } else {
+        alert('Erreur: Vérifier votre saisie');
+    }
 });
 
+
+/**
+ * 
+ * 3 caractères minimum, uniquement des lettres _name 
+ * @param {string}
+ * @returns 
+ */
+function validateName(_name) {
+
+    return true;
+}
+
+function validateBirtDate(_birthDate) {
+
+    return true;
+}
+
+function validatePhoneNumber(_phoneNumber) {
+
+    return true;
+}
+
+function validatePassword(_password1, _password2) {
+
+    return true;
+}
