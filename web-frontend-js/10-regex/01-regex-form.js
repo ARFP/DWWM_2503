24977c1f59876d4c28684d1298ef0471f778ab68
MDevoldere205 <10086604+mdevoldere@users.noreply.github.com>
2025-08-27 14:02:01 +0200

    generateTable

--- web-frontend-js/10-regex/01-regex-form.js
+++ web-frontend-js/10-regex/01-regex-form.js
@@ -11,8 +11,9 @@ myForm.addEventListener('submit', function (event) {
     // controles de saisies
     const isNameValid = validateName(myName.value);
     const isDateValid = validateBirtDate(myBirthDate.value);
+    const isPhoneValid = validatePhoneNumber(myPhone.value);
 
-    if(isNameValid && isDateValid) {
+    if(isNameValid && isDateValid && isPhoneValid) {
         alert('Formulaire valide !');
     } else {
         alert('Erreur: Vérifier votre saisie');
@@ -62,24 +63,36 @@ function validateBirtDate(_birthDate) {
 
 /**
  * Valide un numéro de téléphone
+ * Remplacer les points et les espaces par une chaine vide.
  * 0607080910
  * +33607080910
+ * 01 / 02 / 03 / 04 / 05 / 06 / 07 / 09
  * @param {*} _phoneNumber 
  * @returns 
  */
 function validatePhoneNumber(_phoneNumber) {
 
-    return true;
+    const regexPhone = /^0[1-7|9]\d{8}$/;
+    const regexDotsReplace = /\.|\s/g;
+    
+    _phoneNumber = _phoneNumber.trim();
+    _phoneNumber = _phoneNumber.replaceAll(regexDotsReplace, '');
+
+    /*_phoneNumber = _phoneNumber.replaceAll('.', '');
+    _phoneNumber = _phoneNumber.replaceAll(' ', '');*/
+    //alert(_phoneNumber);
+
+    return regexPhone.test(_phoneNumber);
 }
 
 /**
  * Valider que les 2 mots de passe sont identiques 
  * ET
  * longueur de 12 caractères minimum
- * au moins 1 majuscule
- * au moins 1 minuscule
- * au moins 1 chiffre
- * @param {*} _password1 
+ * au moins 1 majuscule 
+ * au moins 1 minuscule 
+ * au moins 1 chiffre 
+ * @param {*} _password1  
  * @param {*} _password2 
  * @returns 
  */
